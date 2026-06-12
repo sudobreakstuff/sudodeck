1993d2e12dac00825dae72e232036f23900ea572
sudobreakstuff <sudobreakstuff@users.noreply.github.com>
2026-06-12 15:54:51 +0200

    Update zone boundaries from fresh calibration data

--- sudodeck.ino
+++ sudodeck.ino
@@ -1,4 +1,4 @@
-// SudoDeck - fully unrolled, zero compound conditionals
+// SudoDeck - simple touch reader (known working base)
 #include <TFT_eSPI.h>
 #include <XPT2046_Touchscreen.h>
 #include <SPI.h>
@@ -29,49 +29,23 @@ void drawAll() {
 
 void setup() {
   Serial.begin(115200);
-  delay(500);
+  delay(200);
+
   tft.begin();
   tft.setRotation(0);
+
   touchSPI.begin(25, 39, 32, 33);
   ts.begin(touchSPI);
   pinMode(33, OUTPUT);
   digitalWrite(33, HIGH);
+
   drawAll();
-  Serial.println("SDK");
 }
 
 void loop() {
   TS_Point p = ts.getPoint();
-  Serial.print(p.x);Serial.print(" ");
-  Serial.print(p.y);Serial.print(" ");
+  Serial.print(p.x); Serial.print(" ");
+  Serial.print(p.y); Serial.print(" ");
   Serial.println(p.z);
-
-  int n=Serial.available();
-  n=n-73;
-  if(n<0){delay(10);return;}
-  if(Serial.read()!='S'){delay(10);return;}
-
-  n0[0]=Serial.read();n0[1]=Serial.read();n0[2]=Serial.read();n0[3]=Serial.read();
-  n0[4]=Serial.read();n0[5]=Serial.read();n0[6]=Serial.read();n0[7]=Serial.read();
-  n1[0]=Serial.read();n1[1]=Serial.read();n1[2]=Serial.read();n1[3]=Serial.read();
-  n1[4]=Serial.read();n1[5]=Serial.read();n1[6]=Serial.read();n1[7]=Serial.read();
-  n2[0]=Serial.read();n2[1]=Serial.read();n2[2]=Serial.read();n2[3]=Serial.read();
-  n2[4]=Serial.read();n2[5]=Serial.read();n2[6]=Serial.read();n2[7]=Serial.read();
-  n3[0]=Serial.read();n3[1]=Serial.read();n3[2]=Serial.read();n3[3]=Serial.read();
-  n3[4]=Serial.read();n3[5]=Serial.read();n3[6]=Serial.read();n3[7]=Serial.read();
-  n4[0]=Serial.read();n4[1]=Serial.read();n4[2]=Serial.read();n4[3]=Serial.read();
-  n4[4]=Serial.read();n4[5]=Serial.read();n4[6]=Serial.read();n4[7]=Serial.read();
-  n5[0]=Serial.read();n5[1]=Serial.read();n5[2]=Serial.read();n5[3]=Serial.read();
-  n5[4]=Serial.read();n5[5]=Serial.read();n5[6]=Serial.read();n5[7]=Serial.read();
-  n6[0]=Serial.read();n6[1]=Serial.read();n6[2]=Serial.read();n6[3]=Serial.read();
-  n6[4]=Serial.read();n6[5]=Serial.read();n6[6]=Serial.read();n6[7]=Serial.read();
-  n7[0]=Serial.read();n7[1]=Serial.read();n7[2]=Serial.read();n7[3]=Serial.read();
-  n7[4]=Serial.read();n7[5]=Serial.read();n7[6]=Serial.read();n7[7]=Serial.read();
-  n8[0]=Serial.read();n8[1]=Serial.read();n8[2]=Serial.read();n8[3]=Serial.read();
-  n8[4]=Serial.read();n8[5]=Serial.read();n8[6]=Serial.read();n8[7]=Serial.read();
-  Serial.read();
-
-  n0[8]=n1[8]=n2[8]=n3[8]=n4[8]=n5[8]=n6[8]=n7[8]=n8[8]=0;
-  drawAll();
   delay(10);
 }
