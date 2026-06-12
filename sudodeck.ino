f9f08acdeb8c86ad122ed6caf4456083dd912b2c
sudobreakstuff <sudobreakstuff@users.noreply.github.com>
2026-06-12 15:22:01 +0200

    Full sync protocol: web sends 9-name frame to CYD display

--- sudodeck.ino
+++ sudodeck.ino
@@ -1,4 +1,4 @@
-// SudoDeck firmware - ESP32 CYD with web-syncable button labels
+// SudoDeck - fully unrolled, zero compound conditionals
 #include <TFT_eSPI.h>
 #include <XPT2046_Touchscreen.h>
 #include <SPI.h>
@@ -6,93 +6,72 @@ TFT_eSPI tft;
 SPIClass touchSPI(VSPI);
 XPT2046_Touchscreen ts(33, 36);
 
-char names[9][9] = {
-  "PREV", "PLAY", "NEXT",
-  "VOL+", "MUTE", "VOL-",
-  "BACK", "RLD", "FORW"
-};
+char n0[9]="PREV", n1[9]="PLAY", n2[9]="NEXT";
+char n3[9]="VOL+", n4[9]="MUTE", n5[9]="VOL-";
+char n6[9]="BACK", n7[9]="RLD",  n8[9]="FORW";
 
-const int btnX[9] = {18, 105, 192, 18, 105, 192, 18, 105, 188};
-const int btnY[9] = {45, 45, 45, 151, 151, 151, 257, 257, 257};
-const uint16_t btnC[9] = {
-  TFT_RED, TFT_GREEN, TFT_BLUE,
-  TFT_ORANGE, TFT_PURPLE, TFT_CYAN,
-  TFT_YELLOW, TFT_PINK, TFT_DARKGREEN
-};
+int cw=80, rh=106;
 
-int cw = 80;
-int rh = 106;
-
-void drawButton(int idx) {
-  int col = idx % 3;
-  int row = idx / 3;
-  int bx = col * cw;
-  int by = row * rh;
-  tft.fillRect(bx, by, cw, rh, btnC[idx]);
+void drawAll() {
+  tft.fillScreen(TFT_BLACK);
   tft.setTextColor(TFT_WHITE);
   tft.setTextSize(1);
-  tft.drawString(names[idx], btnX[idx], btnY[idx]);
+  tft.fillRect(0,0,cw,rh,TFT_RED);      tft.drawString(n0,18,45);
+  tft.fillRect(80,0,cw,rh,TFT_GREEN);    tft.drawString(n1,105,45);
+  tft.fillRect(160,0,cw,rh,TFT_BLUE);    tft.drawString(n2,192,45);
+  tft.fillRect(0,106,cw,rh,TFT_ORANGE);  tft.drawString(n3,18,151);
+  tft.fillRect(80,106,cw,rh,TFT_PURPLE); tft.drawString(n4,105,151);
+  tft.fillRect(160,106,cw,rh,TFT_CYAN);  tft.drawString(n5,192,151);
+  tft.fillRect(0,212,cw,rh,TFT_YELLOW);  tft.drawString(n6,18,257);
+  tft.fillRect(80,212,cw,rh,TFT_PINK);   tft.drawString(n7,105,257);
+  tft.fillRect(160,212,cw,rh,TFT_DARKGREEN); tft.drawString(n8,188,257);
 }
 
 void setup() {
   Serial.begin(115200);
   delay(500);
-
   tft.begin();
   tft.setRotation(0);
-  tft.fillScreen(TFT_BLACK);
-
   touchSPI.begin(25, 39, 32, 33);
   ts.begin(touchSPI);
   pinMode(33, OUTPUT);
   digitalWrite(33, HIGH);
-
-  for (int i = 0; i < 9; i++) {
-    drawButton(i);
-  }
-
+  drawAll();
   Serial.println("SDK");
 }
 
 void loop() {
   TS_Point p = ts.getPoint();
-  Serial.print(p.x);
-  Serial.print(" ");
-  Serial.print(p.y);
-  Serial.print(" ");
+  Serial.print(p.x);Serial.print(" ");
+  Serial.print(p.y);Serial.print(" ");
   Serial.println(p.z);
 
-  int avail = Serial.available();
-  if (avail == 0) {
-    delay(10);
-    return;
-  }
+  int n=Serial.available();
+  n=n-73;
+  if(n<0){delay(10);return;}
+  if(Serial.read()!='S'){delay(10);return;}
 
-  char cmd = Serial.read();
-  if (cmd != 'U') {
-    delay(10);
-    return;
-  }
+  n0[0]=Serial.read();n0[1]=Serial.read();n0[2]=Serial.read();n0[3]=Serial.read();
+  n0[4]=Serial.read();n0[5]=Serial.read();n0[6]=Serial.read();n0[7]=Serial.read();
+  n1[0]=Serial.read();n1[1]=Serial.read();n1[2]=Serial.read();n1[3]=Serial.read();
+  n1[4]=Serial.read();n1[5]=Serial.read();n1[6]=Serial.read();n1[7]=Serial.read();
+  n2[0]=Serial.read();n2[1]=Serial.read();n2[2]=Serial.read();n2[3]=Serial.read();
+  n2[4]=Serial.read();n2[5]=Serial.read();n2[6]=Serial.read();n2[7]=Serial.read();
+  n3[0]=Serial.read();n3[1]=Serial.read();n3[2]=Serial.read();n3[3]=Serial.read();
+  n3[4]=Serial.read();n3[5]=Serial.read();n3[6]=Serial.read();n3[7]=Serial.read();
+  n4[0]=Serial.read();n4[1]=Serial.read();n4[2]=Serial.read();n4[3]=Serial.read();
+  n4[4]=Serial.read();n4[5]=Serial.read();n4[6]=Serial.read();n4[7]=Serial.read();
+  n5[0]=Serial.read();n5[1]=Serial.read();n5[2]=Serial.read();n5[3]=Serial.read();
+  n5[4]=Serial.read();n5[5]=Serial.read();n5[6]=Serial.read();n5[7]=Serial.read();
+  n6[0]=Serial.read();n6[1]=Serial.read();n6[2]=Serial.read();n6[3]=Serial.read();
+  n6[4]=Serial.read();n6[5]=Serial.read();n6[6]=Serial.read();n6[7]=Serial.read();
+  n7[0]=Serial.read();n7[1]=Serial.read();n7[2]=Serial.read();n7[3]=Serial.read();
+  n7[4]=Serial.read();n7[5]=Serial.read();n7[6]=Serial.read();n7[7]=Serial.read();
+  n8[0]=Serial.read();n8[1]=Serial.read();n8[2]=Serial.read();n8[3]=Serial.read();
+  n8[4]=Serial.read();n8[5]=Serial.read();n8[6]=Serial.read();n8[7]=Serial.read();
+  Serial.read();
 
-  int idx = Serial.read() - '0';
-  avail = Serial.available();
-  if (avail == 0) {
-    delay(10);
-    return;
-  }
-
-  int nc = 0;
-  while (nc < 8) {
-    avail = Serial.available();
-    if (avail == 0) break;
-    char c = Serial.read();
-    if (c == '\n') break;
-    names[idx][nc] = c;
-    nc++;
-  }
-  names[idx][nc] = 0;
-
-  drawButton(idx);
-
+  n0[8]=n1[8]=n2[8]=n3[8]=n4[8]=n5[8]=n6[8]=n7[8]=n8[8]=0;
+  drawAll();
   delay(10);
 }
