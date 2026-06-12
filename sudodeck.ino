184a6902c6b543f9fbd08263ec7b1b5c323dd989
sudobreakstuff <sudobreakstuff@users.noreply.github.com>
2026-06-12 19:36:05 +0200

    Stable direct SPI touch on VSPI default pins (18,19,23) + CS=33

--- sudodeck.ino
+++ sudodeck.ino
@@ -1,10 +1,8 @@
-// SudoDeck - Landscape (320x240)
+// SudoDeck - dedicated VSPI touch, no library crashes
 #include <TFT_eSPI.h>
-#include <XPT2046_Touchscreen.h>
 #include <SPI.h>
 TFT_eSPI tft;
 SPIClass touchSPI(VSPI);
-XPT2046_Touchscreen ts(33, 36);
 
 char n0[9]="PREV", n1[9]="PLAY", n2[9]="NEXT";
 char n3[9]="VOL+", n4[9]="MUTE", n5[9]="VOL-";
@@ -28,19 +26,37 @@ void setup() {
   tft.fillRect(cw,rh*2,cw,rh,TFT_PINK);    tft.drawString(n7,140,190);
   tft.fillRect(cw*2,rh*2,cw,rh,TFT_DARKGREEN); tft.drawString(n8,245,190);
 
-  touchSPI.begin(25, 39, 32, 33);
-  ts.begin(touchSPI);
+  touchSPI.begin();
   pinMode(33, OUTPUT);
   digitalWrite(33, HIGH);
   Serial.println("SDK");
 }
 
 void loop() {
-  TS_Point p = ts.getPoint();
-  Serial.print(p.x);
+  touchSPI.beginTransaction(SPISettings(2000000, MSBFIRST, SPI_MODE0));
+
+  digitalWrite(33, LOW);
+  delayMicroseconds(5);
+  touchSPI.transfer(0xD0);
+  uint16_t x = (((uint16_t)touchSPI.transfer(0x00)) << 4);
+  x = x | (touchSPI.transfer(0x00) >> 4);
+  digitalWrite(33, HIGH);
+
+  delayMicroseconds(5);
+
+  digitalWrite(33, LOW);
+  delayMicroseconds(5);
+  touchSPI.transfer(0x90);
+  uint16_t y = (((uint16_t)touchSPI.transfer(0x00)) << 4);
+  y = y | (touchSPI.transfer(0x00) >> 4);
+  digitalWrite(33, HIGH);
+
+  touchSPI.endTransaction();
+
+  Serial.print(x);
   Serial.print(" ");
-  Serial.print(p.y);
+  Serial.print(y);
   Serial.print(" ");
-  Serial.println(p.z);
+  Serial.println(8190 - x - y);
   delay(30);
 }
