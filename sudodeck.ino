0fd87ce62206dd627e3344e72341040ab5abe7a5
sudobreakstuff <sudobreakstuff@users.noreply.github.com>
2026-06-12 19:28:47 +0200

    Complete rewrite: landscape, clean calibration, working defaults

--- sudodeck.ino
+++ sudodeck.ino
@@ -1,4 +1,4 @@
-// SudoDeck - library-based touch (proven working)
+// SudoDeck - Landscape (320x240)
 #include <TFT_eSPI.h>
 #include <XPT2046_Touchscreen.h>
 #include <SPI.h>
@@ -9,32 +9,29 @@ XPT2046_Touchscreen ts(33, 36);
 char n0[9]="PREV", n1[9]="PLAY", n2[9]="NEXT";
 char n3[9]="VOL+", n4[9]="MUTE", n5[9]="VOL-";
 char n6[9]="BACK", n7[9]="RLD",  n8[9]="FORW";
-int cw=80, rh=106;
-
-void drawAll() {
-  tft.fillScreen(TFT_BLACK);
-  tft.setTextColor(TFT_WHITE); tft.setTextSize(1);
-  tft.fillRect(0,0,cw,rh,TFT_RED);      tft.drawString(n0,18,45);
-  tft.fillRect(80,0,cw,rh,TFT_GREEN);    tft.drawString(n1,105,45);
-  tft.fillRect(160,0,cw,rh,TFT_BLUE);    tft.drawString(n2,192,45);
-  tft.fillRect(0,106,cw,rh,TFT_ORANGE);  tft.drawString(n3,18,151);
-  tft.fillRect(80,106,cw,rh,TFT_PURPLE); tft.drawString(n4,105,151);
-  tft.fillRect(160,106,cw,rh,TFT_CYAN);  tft.drawString(n5,192,151);
-  tft.fillRect(0,212,cw,rh,TFT_YELLOW);  tft.drawString(n6,18,257);
-  tft.fillRect(80,212,cw,rh,TFT_PINK);   tft.drawString(n7,105,257);
-  tft.fillRect(160,212,cw,rh,TFT_DARKGREEN); tft.drawString(n8,188,257);
-}
+int cw=106, rh=80;
 
 void setup() {
   Serial.begin(115200);
   delay(200);
   tft.begin();
-  tft.setRotation(0);
+  tft.setRotation(1);
+  tft.fillScreen(TFT_BLACK);
+  tft.setTextColor(TFT_WHITE); tft.setTextSize(1);
+  tft.fillRect(0,0,cw,rh,TFT_RED);        tft.drawString(n0,28,30);
+  tft.fillRect(cw,0,cw,rh,TFT_GREEN);      tft.drawString(n1,134,30);
+  tft.fillRect(cw*2,0,cw,rh,TFT_BLUE);     tft.drawString(n2,240,30);
+  tft.fillRect(0,rh,cw,rh,TFT_ORANGE);     tft.drawString(n3,28,110);
+  tft.fillRect(cw,rh,cw,rh,TFT_PURPLE);    tft.drawString(n4,134,110);
+  tft.fillRect(cw*2,rh,cw,rh,TFT_CYAN);    tft.drawString(n5,240,110);
+  tft.fillRect(0,rh*2,cw,rh,TFT_YELLOW);   tft.drawString(n6,28,190);
+  tft.fillRect(cw,rh*2,cw,rh,TFT_PINK);    tft.drawString(n7,140,190);
+  tft.fillRect(cw*2,rh*2,cw,rh,TFT_DARKGREEN); tft.drawString(n8,245,190);
+
   touchSPI.begin(25, 39, 32, 33);
   ts.begin(touchSPI);
   pinMode(33, OUTPUT);
   digitalWrite(33, HIGH);
-  drawAll();
   Serial.println("SDK");
 }
 
